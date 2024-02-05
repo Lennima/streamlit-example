24b5826f7e970359c5f9210374caca489bac32a2
Lennima <114233579+Lennima@users.noreply.github.com>
2024-02-05 14:36:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,9 +38,6 @@ with tab1:
     plt.xlabel('Anzahl')
     plt.ylabel('Wetterbedingung')
             
-for index, row in weather_count_df.iterrows():
-            plt.text(row['weather_count'], index, str(row['weather_count']), va='center')
-    
     st.pyplot(fig)
     
 with tab2:
